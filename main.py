45cf84c3d7ccd98f396036cc509345baf68c5bb4
weihua ren <weihua.ren90@gmail.com>
2024-06-03 17:08:22 +0100

    adding apis for get all tasks and get a single task

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+tasks = {
+    1: {"Summary": "First task"},
+    2: {"Summary": "Second task"}
+}
+
+@app.get("/")
+def read_root():
+    return {"Service": "Task Manager"}
+
+@app.get("/tasks")
+def get_all_tasks():
+    return tasks
+
+@app.get("/task/{task_id}")
+def get_task_by_id(task_id):
+    return tasks[int(task_id)]
